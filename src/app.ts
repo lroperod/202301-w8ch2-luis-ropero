c4be9dc4a3be9e4a3d2fe9949762a2cb8db73048
luis <luisrym@gmail.com>
2023-03-09 20:34:59 +0100

    create a custom error handler

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
+import { errorHandler } from './utils/error-handler.js';
 import express from 'express';
 import cors from 'cors';
-import apiRouter from './api/api-router';
+import apiRouter from './api/api-router.js';
+import bodyParser from 'body-parser';
 
 const app = express();
 
 app.use(cors());
-
+app.use(bodyParser.json());
 app.disable('x-powered-by');
 app.get('/', (req, res) => {
   res.json('Server is working!!');
@@ -13,5 +15,5 @@ app.get('/', (req, res) => {
 
 app.use(express.json());
 app.use('/api/v1', apiRouter);
-
+app.use(errorHandler);
 export default app;
